e1fe10a2b85846aa0e3dd830ea42b6ef57652573
Neftalem <dneftalem@gmail.com>
2025-07-29 23:56:37 +0300

    add localStorage

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ const User_name = document.getElementById("User_name");
 const pass = document.getElementById("pass");
 const login_btn = document.getElementById("login_btn");
 
+    window.addEventListener("DOMContentLoaded", () => {
+        const savedUsername = localStorage.getItem("username");
+        const savedPassword = localStorage.getItem("password");
+
+        if (savedUsername) User_name.value = savedUsername;
+        if (savedPassword) pass.value = savedPassword;
+    });
+
 login_btn.addEventListener("click", () => {
 
-    const user_input = User_name.value;
-    const password_input = pass.value;
+    const user_input = User_name.value.trim();
+    const password_input = pass.value.trim();
 
     if(user_input && password_input) {
+
+        localStorage.setItem("username", user_input);
+        localStorage.setItem("password", password_input);
 
         window.location.href="main.html"
 
